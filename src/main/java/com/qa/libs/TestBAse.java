16a7df2514ea69cfa1a5be93521c46dd8cd10d8f
Sakthivel <sakthi@Sakthivels-MacBook-Air.local>
2019-09-19 17:00:26 +0530

    first git commit for my selenium code

---
+++ src/main/java/com/qa/libs/TestBAse.java
@@ -0,0 +1,9 @@
+package com.qa.libs;
+
+public class TestBAse {
+	
+	public void test() {
+		System.out.println("Test method");
+	}
+
+}
---
+++ src/main/java/com/qa/pages/Login.java
@@ -0,0 +1,11 @@
+package com.qa.pages;
+
+public class Login {
+
+	public void loginPage()
+	{
+		System.out.println(" Login Page");
+	}
+	
+	
+}
